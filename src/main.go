91023fe5ba15c393077ce2ae4d97e4300677aecc
Juan Hüttemann <juanfhuttemann@gmail.com>
2019-07-08 17:51:10 -0400

    add print server starting port

--- src/main.go
+++ src/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
 func main() {
 	PORT := setPort() //Read port from config.yml
+
+	fmt.Println("starting server at port" + PORT)
+
 	if err := http.ListenAndServe(PORT, nil); err != nil {
 		panic(err)
 	}
